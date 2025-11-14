60e073627c782a958c6bcdeb424c65cbde3b7f02
Sandeepr2016 <sandeepanchuri204@gmail.com>
2025-11-14 23:47:22 +0530

    1 file modified

--- index.js
+++ index.js
@@ -50,15 +50,68 @@ console.log(b);
 
 // <<<<<<<<<<<<< Creating closure example >>>>>>>>>>>>
 
-function baseFunc(baseNumber) {
-  return (N) => {
-    return baseNumber + N;
+// function baseFunc(baseNumber) {
+//   return (N) => {
+//     return baseNumber + N;
+//   };
+// }
+
+// var addSix = baseFunc(6);
+// var addFive = baseFunc(5);
+
+// output = addSix(10) + addFive(-25);
+
+// appDiv.innerHTML = JSON.stringify(output);
+
+// CLosure is a function which has its own lexical scope / otherway its own memory
+
+// For example, a function returns a function which holds its own memory i.e. default value will be remebered by innerfunction after closExample (i.e. outer function) execution is completed
+
+let closExample = (defaultVal) => {
+  return () => {
+    console.log(defaultVal);
   };
-}
+};
 
-var addSix = baseFunc(6);
-var addFive = baseFunc(5);
+let logHelloAlways = closExample('Hello');
+let logHiAlways = closExample('Hi');
 
-output = addSix(10) + addFive(-25);
+logHelloAlways(); // Hello
+logHiAlways(); // Hi
 
-appDiv.innerHTML = JSON.stringify(output);
+// One more example, we can do a counter inside a inner fucntion, which holds its lexical scope
+
+let counter = () => {
+  let count = 0;
+  return () => {
+    count++;
+    console.log(count);
+  };
+};
+
+counter()(); // 1
+counter()(); // 1 , here counter isn't increased, becoz we are always calling counter and then inner function. to acheive counter, we have to assign returned fucntion and execute it continuously to iterate, like below,
+
+let incCounter = counter(); // now incCounter holds inner function with its lexical scope count
+
+incCounter(); // 1
+incCounter(); // 2, it memorized its count value increased to 1 so in this step, it incremented to 2
+
+// 1 more example, we can create multipier, which can reused.
+
+let multipier = (multiplyBy) => {
+  return (multiplyWith) => {
+    return multiplyBy * multiplyWith;
+  };
+};
+
+let multiplyBySix = multipier(6);
+
+console.log(multiplyBySix(5)); //30, i.e., 6 * 5 = 30 multiplyBySix is a closure which holds its lexical scope i.e. it memorizes multiplyBy as 6
+
+// Same multipier can be reused for other multipier
+
+let multiplyByThree = multipier(3);
+
+console.log(multiplyByThree(5)); // 3 * 5 =15
+console.log(multiplyByThree(6)); // 3 * 6 = 18
